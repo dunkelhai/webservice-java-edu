d1cdfd2a913d10772cb43171d38abf0bc95b92b7
Alysson <3oalysson@gmail.com>
2024-01-05 10:49:34 -0300

    Update EstacionamentoController.java

--- Estacionamento/src/impacta/ead/jp/estacionamento/controle/EstacionamentoController.java
+++ Estacionamento/src/impacta/ead/jp/estacionamento/controle/EstacionamentoController.java
@@ -3,14 +3,14 @@ package impacta.ead.jp.estacionamento.controle;
 import impacta.ead.jp.estacionamento.persistencia.DAOEstacionamento;
 
 /**
- * Classe controladora que coordena todo o fluxo da informação 
+ * Classe controladora que coordena todo o fluxo da informação
  * e eventos do sistema.
  */
 public class EstacionamentoController {
 
 	/**
-	 * Recupera o status das vagas do estacionamento  
-	 * 
+	 * Recupera o status das vagas do estacionamento
+	 *
 	 * @return status atual de vagas ocupadas no estaciomento
 	 */
 	public static int inicializarVagasOcupadas() {
