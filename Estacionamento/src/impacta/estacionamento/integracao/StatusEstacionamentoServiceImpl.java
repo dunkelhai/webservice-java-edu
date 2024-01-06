e2e39c2b44b9ced3ce87f84aa7f87250b4e80b97
Alysson <3oalysson@gmail.com>
2024-01-06 11:10:21 -0300

    Ajustes

--- Estacionamento/src/impacta/estacionamento/integracao/StatusEstacionamentoServiceImpl.java
+++ Estacionamento/src/impacta/estacionamento/integracao/StatusEstacionamentoServiceImpl.java
@@ -3,7 +3,7 @@ package impacta.estacionamento.integracao;
 import impacta.estacionamento.controle.EstacionamentoController;
 
 import javax.jws.WebService;
-
+import javax.xml.ws.Endpoint;
 @WebService(endpointInterface = "impacta.estacionamento.integracao.StatusEstacionamentoService")
 public class StatusEstacionamentoServiceImpl implements StatusEstacionamentoService{
 
@@ -11,5 +11,9 @@ public class StatusEstacionamentoServiceImpl implements StatusEstacionamentoServ
     public StatusBean getStatus() {
         StatusBean bean = new EstacionamentoController().getStatusDia();
         return bean;
+    }
+
+    public static void main(String[] args) {
+        Endpoint.publish("http://127.0.0.1:8888/status?WSDL", new StatusEstacionamentoServiceImpl());
     }
 }
