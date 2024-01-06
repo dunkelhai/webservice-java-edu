c49446fabad5fa370ef9cca4c1e75ca502e764fb
Alysson <3oalysson@gmail.com>
2024-01-06 10:33:00 -0300

    Ajustes

---
+++ Estacionamento/src/impacta/estacionamento/apresentacao/StatusEstacionamentoService.java
@@ -0,0 +1,11 @@
+package impacta.estacionamento.apresentacao;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style = Style.DOCUMENT)
+public interface StatusEstacionamentoService {
+
+}
