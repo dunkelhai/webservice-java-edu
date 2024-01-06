4dc4ccdb423169e70e042cca40de1b7f890a8826
Alysson <3oalysson@gmail.com>
2024-01-06 10:58:19 -0300

    Update StatusBean.java

--- Estacionamento/src/impacta/estacionamento/integracao/StatusBean.java
+++ Estacionamento/src/impacta/estacionamento/integracao/StatusBean.java
@@ -8,7 +8,31 @@ public class StatusBean {
 
     @XmlElement
     private int vagasLivres;
-    
+
     @XmlElement
     private int vagasOcupadas;
+
+    @XmlElement
+    private double faturamentoDia;
+
+    public StatusBean() {
+    }
+
+    public StatusBean(int vagasLivres, int vagasOcupadas, double faturamentoDia) {
+        this.vagasLivres = vagasLivres;
+        this.vagasOcupadas = vagasOcupadas;
+        this.faturamentoDia = faturamentoDia;
+    }
+
+    public int getVagasLivres() {
+        return vagasLivres;
+    }
+
+    public int getVagasOcupadas() {
+        return vagasOcupadas;
+    }
+
+    public double getFaturamentoDia() {
+        return faturamentoDia;
+    }
 }
